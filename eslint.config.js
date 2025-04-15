b2c77a0e91e590e96f26ca269c765a4330d1d2a7
Afonso <afonsoestevao04@gmail.com>
2025-04-15 20:53:08 -0300

    feat: add eslint.config.js for ESLint 9 compatibility

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      semi: 'error',
+      quotes: ['error', 'single']
+    }
+  }
+];
